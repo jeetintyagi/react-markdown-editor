bae867525dae39a2be5832968da564e87528bcdd
jeetintyagi <jt9999709701@gmail.com>
2020-08-01 20:21:04 +0530

    refactoring done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import  ReactMarkdown  from "react-markdown";
+// const ReactMarkdown = require('react-markdown')
 
-function App() {
+export default function App() {
+  const [markdown, setMarkdown] = useState("Markdown goes here");
+  function handleChange(e) {
+    setMarkdown(e.target.value);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <textarea onChange={handleChange} value={markdown} />
+
+      <ReactMarkdown className="preview" source={markdown} />
     </div>
   );
 }
-
-export default App;
